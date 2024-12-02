ca48793c71d333487c672a1d773f8dfd74878bda
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-12-03 03:32:38 +0630

    feat: Web Api with EFCore

---
+++ DotNetBatch14KZT.RestApi/Features/Blog/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetBatch14KZT.RestApi.Features.Blog;
+
+public class AppDbContext : DbContext
+{
+    private readonly SqlConnectionStringBuilder _connectionBuilder;
+
+    public AppDbContext()
+    {
+
+        _connectionBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "test_db",
+            UserID = "sa",
+            Password = "Kyawzin@123",
+            TrustServerCertificate = true,
+        };
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_connectionBuilder.ConnectionString);
+        }
+    }
+    public DbSet<BlogModel> Blogs { get; set; }
+
+}
---
+++ DotNetBatch14KZT.RestApi/Features/Blog/BlogEFCoreService.cs
@@ -0,0 +1,131 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetBatch14KZT.RestApi.Features.Blog;
+
+public class BlogEFCoreService : IBlogService
+{
+    private readonly AppDbContext _db;
+
+    public BlogEFCoreService()
+    {
+        _db = new AppDbContext();
+    }
+
+    public BlogResponseModel CreateBlog(BlogModel requestModel)
+    {
+        _db.Add(requestModel);
+        var result = _db.SaveChanges();
+
+        string message = result > 0 ? "Create Success." : "Create Fail!";
+        BlogResponseModel model = new BlogResponseModel();
+        model.IsSuccess = result > 0;
+        model.Message = message;
+        return model;
+    }
+
+    public BlogResponseModel DeleteBlog(string id)
+    {
+        var item = _db.Blogs.AsNoTracking().FirstOrDefault(x => x.blog_id == id);
+        if(item is null)
+        {
+            return new BlogResponseModel
+            {
+                IsSuccess = false,
+                Message = "No data found."
+            };
+        }
+
+        _db.Entry(item).State = EntityState.Deleted;
+        int result = _db.SaveChanges();
+
+        string message = result > 0 ? "Delete Success." : "Delete Fail!";
+        BlogResponseModel model = new BlogResponseModel();
+        model.IsSuccess = result > 0;
+        model.Message = message;
+
+        return model;
+    }
+
+    public BlogModel GetBlog(string id)
+    {
+        var item = _db.Blogs.AsNoTracking().FirstOrDefault(x => x.blog_id == id);
+        return item!;
+    }
+
+    public List<BlogModel> GetBlogs()
+    {
+        List<BlogModel> lst = _db.Blogs.AsNoTracking().ToList();
+        return lst;
+    }
+
+    public BlogResponseModel UpdateBlog(BlogModel requestModel)
+    {
+        var item = _db.Blogs.AsNoTracking().FirstOrDefault(x => x.blog_id == requestModel.blog_id);
+        if(item is null)
+        {
+            return new BlogResponseModel
+            {
+                IsSuccess = false,
+                Message = "No data found."
+            };
+        }
+
+        if (string.IsNullOrEmpty(requestModel.blog_title))
+        {
+            requestModel.blog_title = item.blog_title;
+        }
+        if(string.IsNullOrEmpty(requestModel.blog_author))
+        {
+            requestModel.blog_author = item.blog_author;
+        }
+        if (string.IsNullOrEmpty(requestModel.blog_content))
+        {
+            requestModel.blog_content = item.blog_content;
+        }
+
+        _db.Entry(item).State = EntityState.Modified;
+        var result = _db.SaveChanges();
+
+        string message = result > 0 ? "Update Success." : "Update Fail!";
+        BlogResponseModel model = new BlogResponseModel();
+        model.IsSuccess = result > 0;
+        model.Message = message;
+
+        return model;
+    }
+
+    public BlogResponseModel UpsertBlog(BlogModel requestModel)
+    {
+        BlogResponseModel model = new BlogResponseModel();
+        var item = _db.Blogs.AsNoTracking().FirstOrDefault(x => x.blog_id == requestModel.blog_id);
+        if(item is null)
+        {
+            model = CreateBlog(requestModel);
+            return model;
+        }
+
+        if (string.IsNullOrEmpty(requestModel.blog_title))
+        {
+            requestModel.blog_title = item.blog_title;
+        }
+        if (string.IsNullOrEmpty(requestModel.blog_author))
+        {
+            requestModel.blog_author = item.blog_author;
+        }
+        if (string.IsNullOrEmpty(requestModel.blog_content))
+        {
+            requestModel.blog_content = item.blog_content;
+        }
+
+        _db.Entry(item).State = EntityState.Modified;
+        var result = _db.SaveChanges();
+
+        string message = result > 0 ? "Update Success." : "Update Fail!";
+        model.IsSuccess = result > 0;
+        model.Message = message;
+
+        return model;
+
+    }
+}
