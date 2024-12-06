3298a293d850530aabf0fe802224bd25bf79862b
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-12-06 22:56:45 +0630

    feat: Mini POS

---
+++ MiniPosSystemKZT.RestApi/Features/POS/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniPosSystemKZT.RestApi.Features.POS;
+
+public class AppDbContext : DbContext
+{
+    private readonly SqlConnectionStringBuilder _conntectionBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = ".",
+        InitialCatalog = "mini_pos_db",
+        UserID = "sa",
+        Password = "Kyawzin@123",
+        TrustServerCertificate = true,
+    };
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_conntectionBuilder.ConnectionString);
+        }
+    }
+    public DbSet<ProductModel>? Product { get; set; }
+    public DbSet<TransactionModel>? Transaction { get; set; }
+}
