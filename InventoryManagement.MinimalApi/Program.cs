d5a1b1b0c25ae5df3933d2d84568b520f8539cae
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2024-12-31 21:11:52 +0630

    feat: add minimalApi

---
+++ InventoryManagement.MinimalApi/Program.cs
@@ -0,0 +1,36 @@
+using InventoryManagement.Shared.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+ProductService productService = new ProductService();
+
+app.MapGet("/api/Product", () =>
+{
+    var model = productService.GetProducts();
+    return Results.Ok(model);
+})
+    .WithName("GetProducts")
+    .WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
