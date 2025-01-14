ce9d7b3b737eb3f3489e34a4457d2cb35aa87ae2
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2025-01-14 22:07:30 +0630

    feat: add MVC

---
+++ DotNetCore.MVC/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using DotNetBatch14KZT3.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.MVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _service;
+
+        public BlogController()
+        {
+            _service = new BlogEFCoreService();
+        }
+
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            var lst = _service.GetBlogs();
+
+            return View("BlogIndex", lst);
+        }
+    }
+}
