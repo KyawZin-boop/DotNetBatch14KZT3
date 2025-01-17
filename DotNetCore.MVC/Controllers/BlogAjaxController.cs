2e75d5b6e47961725d262382fb5086079d38aa3f
Kyaw Zin Thant <kyawzinthant57@gmail.com>
2025-01-17 22:17:51 +0630

    feat: mvc + ajax

--- DotNetCore.MVC/Controllers/BlogAjaxController.cs
+++ DotNetCore.MVC/Controllers/BlogAjaxController.cs
@@ -27,5 +27,45 @@ namespace DotNetCore.MVC.Controllers
 
             return Json(blogs);
         }
+
+        [ActionName("CreateAjax")]
+        public async Task<IActionResult> CreateBlog()
+        {
+            return View("CreateBlogAjax", new BlogModel());
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> SaveBlog(BlogDTO requestModel)
+        {
+            var response = await _service.CreateBlog(requestModel);
+
+            return Json(response.Message);
+        }
+
+        [ActionName("Edit")]
+        public async Task<IActionResult> EditBlog(string id)
+        {
+            var blog = await _service.GetBlog(id);
+
+            return View("EditBlogAjax", blog);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public async Task<IActionResult> UpdateBlog(BlogModel model)
+        {
+            var response = await _service.UpdateBlog(model);
+
+            return Json(response);
+        }
+
+        [ActionName("DeleteBlogAjax")]
+        public async Task<IActionResult> DeleteBlog(string id)
+        {
+            var response = await _service.DeleteBlog(id);
+
+            return Json(response.Message);
+        }
     }
 }
